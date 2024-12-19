06d8fa41b2ec8e12a682a36cedca4b0f3e052d54
kshatriya1502 <kshatriyaaditya1502@gmail.com>
2024-12-19 21:42:16 +0530

    modified

--- testmodule1.js
+++ testmodule1.js
@@ -4,3 +4,8 @@ const math = require("./lecture_1/test_module/hello") ;
 
 console.log(math.add(1,2));
 console.log(math.sub(1,2));
+
+// this is the test file for the git 
+for(let i = 0 ;i < 10; i ++){
+    console.log("hello world") ; 
+}
